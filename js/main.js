09e99b91454ee2c041690241a48217b8fdc9f18d
Putri Dinar <79742186+putridinar@users.noreply.github.com>
2025-03-08 17:12:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,32 @@
 simpleCart({
-
   checkout: {
-    // type: "PayPal",
-    // email: "you@yours.com",
+    type: "PayPal",
+    email: "you@yours.com",
   },
 
-  // tax: 0.075,
-  // currency: "THB",
-
-  cartStyle: "table",
+  tax: 0.05, // 5% tax
+  
+  cartStyle: "div", // Pastikan ada elemen <div>
 
   cartColumns: [
-    { attr: "name" , label: "Name" } ,
-    { attr: "price" , label: "Price", view: 'currency' } ,
-    { attr: "size" , label: "Size" } ,
-    { attr: "color" , label: "Color" } ,
-    { view: "decrement" , label: false , text: "- 1" } ,
-    { attr: "quantity" , label: "Qty" } ,
-    { view: "increment" , label: false , text: "+ 1" } ,
-    { attr: "total" , label: "SubTotal", view: 'currency' } ,
-    { view: "remove" , text: "Remove" , label: false }
+    { attr: "name", label: "Name" },
+    { attr: "price", label: "Price", view: "currency" },
+    { attr: "size", label: "Size" },
+    { attr: "color", label: "Color" },
+    { view: "decrement", label: false, text: "- 1" },
+    { attr: "quantity", label: "Qty" },
+    { view: "increment", label: false, text: "+ 1" },
+    { attr: "total", label: "SubTotal", view: "currency" },
+    { view: "remove", text: "Remove", label: false },
   ]
-
 });
 
-// simpleCart.currency({
-  // code: "THB",
-  // name: "Thai Baht",
-  // symbol: "&#3647;",
-  // delimiter: " ",
-  // decimal: ",",
-  // after: true,
-  // accuracy: 0
-// });
-
-//* Refresh cart once simpleCart is ready to listen.
-simpleCart.ready(function() {
-  simpleCart.update();
+simpleCart.currency({
+  code: "IDR",
+  name: "Indonesian Rupiah",
+  symbol: "Rp",
+  delimiter: ".",
+  decimal: ",",
+  after: false, // Agar formatnya "Rp 1.000" bukan "1.000 Rp"
+  accuracy: 0
 });

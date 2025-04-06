f97ffef0073dd8dd3db1a84a97a2f6ccac31d393
Putri Dinar <79742186+putridinar@users.noreply.github.com>
2025-04-06 15:10:51 +0700

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,7 +1,7 @@
 zsimpleCart.ready(function() {
   simpleCart.each(function(item, x) {
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Name" value="' + item.get('name') + '">');
-    $('form').append('<input type="hidden" name="' + item.get('id') + ' Wrna" value="' + item.get('warna') + '">');
+    $('form').append('<input type="hidden" name="' + item.get('id') + ' Warna" value="' + item.get('warna') + '">');
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Varian" value="' + item.get('varian') + '">');
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Qty" value="' + item.get('quantity') + '">');
     $('form').append('<input type="hidden" name="' + item.get('id') + ' Price" value="' + item.get('price') + '">');
